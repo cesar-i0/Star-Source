396ae5a38a72fcca0bad656ac3a0f9fd1be22ce5
Janielly <janiellybenicio94@gmailcom>
2025-12-27 19:47:34 -0300

    Versão 0.8

--- src/main/ManipuladorDeEvento.java
+++ src/main/ManipuladorDeEvento.java
@@ -1,89 +1,112 @@
 package main;
 
-import java.awt.Rectangle;
-
 public class ManipuladorDeEvento {
 
-    PainelDoJogo pj;
-    Rectangle evento_com_rectangulo;
-    int evento_com_rectangulo_padraoX, evento_com_rectangulo_padraoY;
+PainelDoJogo pj;
+ReacaoDeEvento reacaoDeEvento[][];
 
-    public ManipuladorDeEvento(PainelDoJogo pj){
+int EventoPrevioX, EventoPrevioY;
+boolean eventoAtivo = true;
+public ManipuladorDeEvento(PainelDoJogo pj){
 
-        this.pj = pj;
+this.pj = pj;
 
-        evento_com_rectangulo = new Rectangle();
-        evento_com_rectangulo.x = 23;
-        evento_com_rectangulo.y = 23;
-        evento_com_rectangulo.width = 2;
-        evento_com_rectangulo.height = 2;
-        evento_com_rectangulo_padraoX = evento_com_rectangulo.x;
-        evento_com_rectangulo_padraoY = evento_com_rectangulo.y;
+reacaoDeEvento = new ReacaoDeEvento[pj.maxColunasDoMundo][pj.maxLinhaDoMundo];
 
-    }
+int col = 0;
+int row = 0;
 
-    public void verificaEvento(){
+while (col < pj.maxColunasDoMundo && row < pj.maxLinhaDoMundo){
+reacaoDeEvento [col][row]= new ReacaoDeEvento();
+reacaoDeEvento [col][row].x = 23;
+reacaoDeEvento [col][row].y = 23;
+reacaoDeEvento[col][row].width = 2;
+reacaoDeEvento[col][row].height = 2;
+reacaoDeEvento[col][row].x = reacaoDeEvento[col][row].x;
+reacaoDeEvento[col][row].y = reacaoDeEvento[col][row].y;
 
-        // Local da armadilha
-        if(atingiu(1,1, "cima") == true) danoDeArmadilha(pj.estado_de_dialogo);
-        // Local de cura
-        if(atingiu(3, 1, "cima") == true) localDeCura(pj.estado_de_dialogo);
-        // Local de teleporte
-        if(atingiu(5, 1, "cima") == true) teleporte(pj.estado_de_dialogo);
+col++;
+if(col == pj.maxColunasDoMundo){
+col = 0;
+row++;
+}
+}
 
-    }
+}
 
-    public boolean atingiu(int eventoCol, int enventoLin, String direcaoRequerida){
-        
-        boolean atingiu = false;
+public void verificaEvento(){
 
-        pj.jogador.area_solida.x = pj.jogador.mundoX + pj.jogador.area_solida.x;
-        pj.jogador.area_solida.y = pj.jogador.mundoY + pj.jogador.area_solida.y;
+int xDistancia = Math.abs(pj.jogador.mundoX - EventoPrevioX);
+int yDistancia = Math.abs(pj.jogador.mundoY - EventoPrevioY);
+int distancia = Math.max(xDistancia, yDistancia);
+if (distancia > pj.tamanhoDaPeca) {
+eventoAtivo = true;
+}
+if (eventoAtivo == true){
+// Local da armadilha
+if(atingiu(1, 1, "cima") == true) danoDeArmadilha(1, 1,pj.estado_de_dialogo);
+// Local de cura
+if(atingiu(23, 12, "cima") == true) localDeCura(23, 12,pj.estado_de_dialogo);
+// Local de teleporte
+if(atingiu(5, 1, "cima") == true) teleporte(5, 1, pj.estado_de_dialogo);
 
-        evento_com_rectangulo.x = eventoCol * pj.tamanhoDaPeca + evento_com_rectangulo.x;
-        evento_com_rectangulo.y = enventoLin * pj.tamanhoDaPeca + evento_com_rectangulo.y;
+}
+}
 
-        if(pj.jogador.area_solida.intersects(evento_com_rectangulo)){
-            if(pj.jogador.direcao.contentEquals(direcaoRequerida) || direcaoRequerida.contentEquals("any")){
-                atingiu = true;
-            }
-        }
+public boolean atingiu(int col, int row, String direcaoRequerida){
+boolean atingiu = false;
 
-        pj.jogador.area_solida.x = pj.jogador.area_solida_padraoX;
-        pj.jogador.area_solida.y = pj.jogador.area_solida_padraoY;
-        evento_com_rectangulo.x = evento_com_rectangulo_padraoX;
-        evento_com_rectangulo.y = evento_com_rectangulo_padraoY;
+pj.jogador.area_solida.x = pj.jogador.mundoX + pj.jogador.area_solida_padraoX;
+pj.jogador.area_solida.y = pj.jogador.mundoY + pj.jogador.area_solida_padraoY;
 
-        return atingiu;
+reacaoDeEvento[col][row].x = col * pj.tamanhoDaPeca + reacaoDeEvento[col][row].x;
+reacaoDeEvento[col][row].y = row * pj.tamanhoDaPeca + reacaoDeEvento[col][row].y;
 
-    }
+if(pj.jogador.area_solida.intersects(reacaoDeEvento[col][row]) && reacaoDeEvento[col][row].evento_feito == false){
+if(pj.jogador.direcao.contentEquals(direcaoRequerida) || direcaoRequerida.contentEquals("any")){
+atingiu = true;
 
-    public void teleporte(int estado_do_jogo){
+EventoPrevioX = pj.jogador.mundoX;
+EventoPrevioY = pj.jogador.mundoY;
+}
+}
 
-        pj.estado_do_jogo = estado_do_jogo;
-        pj.ui.dialogo_atual = "Teleporte!";
-        pj.jogador.mundoX = pj.tamanhoDaPeca * 40;
-        pj.jogador.mundoY = pj.tamanhoDaPeca * 25;
+pj.jogador.area_solida.x = pj.jogador.area_solida_padraoX;
+pj.jogador.area_solida.y = pj.jogador.area_solida_padraoY;
+reacaoDeEvento[col][row].x = reacaoDeEvento[col][row].x;
+reacaoDeEvento[col][row].y = reacaoDeEvento[col][row].y;
 
-    }
+return atingiu;
 
-    public void danoDeArmadilha(int estado_do_jogo){
+}
 
-        pj.estado_do_jogo = estado_do_jogo;
-        pj.ui.dialogo_atual = "Você caiu em uma armadilha!";
-        pj.jogador.vida -= 1;
+public void teleporte(int col, int row, int estado_do_jogo){
 
-    }
+pj.estado_do_jogo = estado_do_jogo;
+pj.ui.dialogo_atual = "Teleporte!";
+pj.jogador.mundoX = pj.tamanhoDaPeca * 40;
+pj.jogador.mundoY = pj.tamanhoDaPeca * 25;
+//reacaoDeEvento[col][row].evento_feito = true;
+eventoAtivo = false;
+}
 
-    public void localDeCura(int estado_do_jogo){
+public void danoDeArmadilha(int col, int row,int estado_do_jogo){
 
-        System.out.println("Curando!");
+pj.estado_do_jogo = estado_do_jogo;
+pj.ui.dialogo_atual = "Você caiu em uma armadilha!";
+pj.jogador.vida -= 1;
 
-        if(pj.chaveManipuladora.enterPressionado == true){
-            pj.estado_do_jogo = estado_do_jogo;
-            pj.ui.dialogo_atual = "Você se curou!\nSua vida foi restaurada.";
-            pj.jogador.vida = pj.jogador.vidaMaxima;
-        }
-    }
+}
+
+public void localDeCura(int col, int row, int estado_do_jogo){
+
+System.out.println("Curando!");
+
+if(pj.chaveManipuladora.enterPressionado == true){
+pj.estado_do_jogo = estado_do_jogo;
+pj.ui.dialogo_atual = "Você se curou!\nSua vida foi restaurada.";
+pj.jogador.vida = pj.jogador.vidaMaxima;
+}
+}
 
 }
---
+++ src/main/ReacaoDeEvento.java
@@ -0,0 +1,14 @@
+
+
+
+package main;
+
+import java.awt.Rectangle;
+
+public class ReacaoDeEvento extends Rectangle{
+
+int evento_com_rectangulo_padraoX, evento_com_rectangulo_padraoY;
+
+boolean evento_feito = false;
+
+}
