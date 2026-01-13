fa175f6b48456ca36dfe6a85944dc8b028bda442
cesar.io <cesario.0324@hotmail.com>
2026-01-13 10:01:08 -0300

    Versão 4.7

---
+++ src/objetos/OBJ_PocaoDeMana.java
@@ -0,0 +1,52 @@
+package objetos;
+
+import main.PainelDoJogo;
+import entidades.Entidade;
+import java.awt.Rectangle;
+
+public class OBJ_PocaoDeMana extends SuperClasse {
+
+     PainelDoJogo pj;
+
+    public OBJ_PocaoDeMana (PainelDoJogo pj){
+        
+        super(pj);
+        tipo = tipo_consumivel;
+        super.nome = "Porção de mana";
+
+        this.pj = pj;
+
+        getImagem();
+
+        descricao = "[" + nome + "]\n" + "Recupera sua mana em " + valor + ".";
+
+        area_solida = new Rectangle();
+        area_solida.x = 10;
+        area_solida.y = 8;
+        area_solida.width = 30;
+        area_solida.height = 30;
+        area_solida_padraoX = area_solida.x;
+        area_solida_padraoY = area_solida.y;
+    }
+
+    @Override
+    public void getImagem() {
+        estatico = configuracoes("/res/objetos/pocaoM", pj.tamanhoDaPeca, pj.tamanhoDaPeca);
+    }
+
+    @Override
+    public boolean use(Entidade entidades){
+        pj.estado_do_jogo = pj.estado_de_dialogo;
+        pj.ui.dialogo_atual = "Você bebeu " + nome + "!\n" + "Você recuperou " + valor + " de mana.";
+        entidades.vida += valor;
+
+        if(pj.jogador.mana > pj.jogador.mana_max){
+            pj.jogador.mana = pj.jogador.mana_max;
+        }
+        //Adicionar som
+        return true;
+    }
+
+    
+
+}
